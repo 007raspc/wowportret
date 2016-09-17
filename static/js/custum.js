aac875720136c95c91ebf79efdabd7882f3b3d67
007raspc <007raspc@gmail.com>
2016-09-18 02:07:34 +0300

    Comm 18.09.2016
    Поигрался со стилями главного меню, заменил картинки на оптемизированные

--- static/js/custum.js
+++ static/js/custum.js
@@ -4,3 +4,7 @@ jQuery(function(f){
         element['fade'+ (f(this).scrollTop() > 200 ? 'In': 'Out')](500);           
     });
 });
+
+$( "#target" ).click(function() {
+  alert( "Handler for .click() called." );
+});
